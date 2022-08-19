24967e67e78986f45d687cb45f75b1f99b52ca67
petrenko-vel <85351552+petrenko-vel@users.noreply.github.com>
2022-08-19 18:06:37 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
     function many(state){
         document.getElementById('windows-many').style.display = state;	
-        document.getElementById('gray').style.display = state; 		
+        document.getElementById('many-gray').style.display = state; 		
 	}
 
 
     function shop(state){
         document.getElementById('windows-shop').style.display = state;	
-        document.getElementById('gray').style.display = state; 		
+        document.getElementById('shop-gray').style.display = state; 		
 	}
 
     function lend(state) {
         document.getElementById('windows-lend').style.display = state;	
-        document.getElementById('gray').style.display = state; 		
+        document.getElementById('lend-gray').style.display = state; 		
 	}
 
 
